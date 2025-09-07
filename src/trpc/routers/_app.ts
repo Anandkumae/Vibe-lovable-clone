229ae9193392a92a9727450e7fcd81cf466ed8f8
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2025-09-07 21:11:51 +0530

    new

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
+export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(
+      z.object({
+        value: z.string()
+      })
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.worlk",
+        data: {
+          value: input.value,
+        }
+      })
+    }),
+  createAI: baseProcedure
+      .input(
+        z.object({
+          text: z.string(),
+        })
+      )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+});
+export type AppRouter = typeof appRouter;
