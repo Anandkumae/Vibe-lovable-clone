595d497a89ea6bb7b1d6fe68ef60c0f7a90f36ee
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2025-09-13 03:43:27 +0530

    theme

---
+++ src/modules/home/ui/components/navbar.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button"
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+
+
+
+
+
+
+export const Navbar = () => {
+
+    return (
+        <nav
+            className="p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all duration-200 border-b border-transparent "
+        >
+            <div>
+                <Link href={"/"} className="flex items-center gap-2">
+                    <Image src={"/logo.svg"} alt="vibe" />
+                    <span className="font-semibold text-lg">Vibe</span>
+                </Link>
+                <SignedOut>
+                    <SignUpButton>
+                        <Button variant="outline" size="sm">
+                            Sign up
+                        </Button>
+                    </SignUpButton>
+                    <SignInButton>
+                        <Button variant="outline" size="sm">
+                            Sign in
+                        </Button>
+                    </SignInButton> 
+                </SignedOut>
+                <SignedIn>
+                    <p>TODO: </p>
+                </SignedIn>
+
+            </div> 
+        </nav>
+    )
+}
